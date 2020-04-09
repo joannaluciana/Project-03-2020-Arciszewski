a9b8361fc786b0f405c36e3154118703ba3c4deb
joannaluciana <joanna@wloskowicz.pl>
2020-04-09 13:03:31 +0200

    komit zmian i zepsutego indeksu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let navigation = document.getElementById("navigation");
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   if (window.pageYOffset > 0) {
     navigation.classList.add("active");
   } else {
@@ -8,9 +8,13 @@ window.addEventListener("scroll", function() {
 });
 
 let hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
+hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("open");
   document.body.classList.toggle("menu-open");
+});
+let text_1 = document.querySelector(".gallery-txt");
+text_1.hover(function () {
+  this.removeClass("hover");
 });
 
 AOS.init();
